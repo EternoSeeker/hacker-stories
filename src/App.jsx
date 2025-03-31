5b0e0d131bd735408722baa2d09dcc196d13ae3c
Shrihari Magar <shriharimagar1@gmail.com>
2025-03-31 17:49:56 +0530

    custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
+import * as React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const useStorageState = (key, initialState) => {
+  const [value, setvalue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem("value", value);
+  }, [value, key]);
+
+  return [value, setvalue];
+};
+
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://react.dev/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [value, setvalue] = useStorageState("search", "React");
+
+  const handleSearch = (event) => {
+    setvalue(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(value.toLowerCase())
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>My Hacker Stories</h1>
+      <h2>Here's the test component</h2>
+      <Search search={value} onSearch={handleSearch} />
+      <hr />
+      <List list={searchedStories} />
+    </div>
+  );
+};
 
-export default App
+const Search = ({ onSearch, search }) => {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" value={search} onChange={onSearch} />
+    </div>
+  );
+};
+
+const List = ({ list }) => {
+  return (
+    <ul>
+      {list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  );
+};
+
+export default App;
